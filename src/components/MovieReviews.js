62855c95885f627cd4b269d9cdaffbd2d42093fc
sondos alansi <sondos.alansi@yahoo.com>
2018-11-26 05:56:03 +0000

    automatically backed up by learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,11 +1,18 @@
 // Code MovieReviews Here
 import React, { Component } from 'react';
 
-const MovieReviews=(props) =>
-<div className='review-list'>
-{props.reviews.map(review => li key={review.display_title} className="review">
-{review.display_title}
-</li>
-)}
-</div>
+const Review = ({headline}) => {
+  return (
+    <div className="review">
+      {headline}
+    </div>
+  );
+};
+
+const MovieReviews = ({ reviews }) => <div className="review-list">{reviews.map(Review)}</div>;
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
 
