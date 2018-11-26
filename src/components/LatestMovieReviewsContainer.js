b9b4712f425893327f2c301af24e3c7d9d81b7e4
sondos alansi <sondos.alansi@yahoo.com>
2018-11-26 05:50:15 +0000

    automatically backed up by learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,3 +6,17 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends React.Component {
+
+state={
+  reviews:[]
+}
+
+  componentDidMount() {
+     fetch(URL)
+       .then(response => response.json())
+       .then(movies => this.setState({ reviews:movies.results }))
+   }
+
+  
+}
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
 // Code MovieReviews Here
+import React, { Component } from 'react';
+
+const MovieReviews=(props) =>
+<div className='review-list'>
+{props.reviews.map(review => li key={review.display_title} className="review">
+{review.display_title}
+</li>
+)}
+</div>
+
